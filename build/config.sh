1b4e3c15bd4587a229945d779997cb73257309cf
hsiaofongw <hsiaofong.w@gmail.com>
2024-07-15 02:55:06 +0800

    Update build script

--- build/config.sh
+++ build/config.sh
@@ -4,4 +4,8 @@ scriptDir=$(dirname $0)
 prefixDir=$HOME/.local/coreutils
 mkdir -p $prefixDir
 
-$scriptDir/../../configure --prefix=$prefixDir --with-linux-crypto=no --with-openssl=no
+configureExec=$scriptDir/../../configure
+$configureExec --prefix=$prefixDir --with-linux-crypto=no --with-openssl=no
+
+makefileDir=$(dirname $configureExec)
+cd $makefileDir && make lib/byteswap.h
