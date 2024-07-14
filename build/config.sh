b8c5fa80f41d25460f82fb51c8b1cc2353b451fa
hsiaofongw <hsiaofong.w@gmail.com>
2024-07-15 00:07:55 +0800

    Update configure launcher script

--- build/config.sh
+++ build/config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 scriptDir=$(dirname $0)
+prefixDir=$HOME/.local/coreutils
+mkdir -p $prefixDir
 
-$scriptDir/../../configure --with-linux-crypto=no --with-openssl=no
+$scriptDir/../../configure --prefix=$prefixDir --with-linux-crypto=no --with-openssl=no
